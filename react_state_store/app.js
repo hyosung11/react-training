64b5d18c366173cb538e82cfdf90b3bb0ede782a
HyoSung Bidol-Lee <hyosung11@me.com>
2019-12-03 20:48:41 -0500

    react store

--- react_state_store/app.js
+++ react_state_store/app.js
@@ -8,25 +8,80 @@ class App extends React.Component {
       name: '',
       price: 0,
       description: 'Describe this item',
-      value: ''
+      isHiring: true
     }
     this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.toggleHiring = this.toggleHiring.bind(this)
   }
   handleChange(event) {
     this.setState({[event.target.id]: event.target.value})
   }
+  handleSubmit(event) {
+    event.preventDefault()
+    const newItem = {
+      name: this.state.name,
+      price: this.state.price,
+      description: this.state.description
+    }
+    this.setState({
+      products: [ newItem, ...products ],
+      name: '',
+      price: 0,
+      description: 'Describe this item'
+    })
+  }
+  toggleHiring() {
+    this.setState({isHiring: !this.state.isHiring})
+  }
   render () {
     return (
       <div>
-        <h1> Big Time Shopping </h1>
-        <form>
+        <h1 onClick={this.toggleHiring}> Big Time Shopping </h1>
+          {!this.state.isHiring ? <h2>Yes, we are hiring</h2> : <h2>Sorry, try again tomorrow</h2>}
+        <form onSubmit={this.handleSubmit}>
           <label htmlFor="name">Name</label>
-          <input type="text" value={this.state.name} onChange={this.handleChange} id="name" placeholder='name of product'/>
+          <input
+            type="text"
+            value={this.state.name}
+            onChange={this.handleChange}
+            id="name"
+            placeholder='name of product'
+          />
+          <br />
+
+          <label htmlFor="price">Price</label>
+          <input
+            type="number"
+            value={this.state.price}
+            onChange={this.handleChange}
+            id="price"
+          />
+          <br />
+
+          <label htmlFor="description">Description</label>
+          <input
+            type="text"
+            value={this.state.description}
+            onChange={this.handleChange}
+            id="description"
+            placeholder='describe item'
+          />
+          <br />
+          <input type="submit" />
         </form>
+        <div>
+          <h2>Preview This Item</h2>
+          <h3>{this.state.name}</h3>
+          <h4>{this.state.price}</h4>
+          <h5>{this.state.description}</h5>
+        </div>
         <ul>
           {this.state.products.map(product => {
             return (
-              <li>{product.name} {product.price}</li>)}
+              <li>
+                {product.name} {product.price}
+              </li>)}
             )
           }
         </ul>
